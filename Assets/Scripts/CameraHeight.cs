cd9da00b35d52b4ae548abcf7e44383e7f3391d2
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-10-23 20:44:52 -0200

    Inserção do glow

---
+++ Assets/Scripts/CameraHeight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHeight : MonoBehaviour {
+
+	float speed = 6f;
+	float height = 0.1f;
+
+	Vector3 target;
+
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.KeypadPlus)){
+			target = new Vector3(transform.position.x, transform.position.y + height, transform.position.z);
+			transform.position = Vector3.Lerp (transform.position, target, speed * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.KeypadMinus)){
+			target = new Vector3(transform.position.x, transform.position.y - height, transform.position.z);
+			transform.position = Vector3.Lerp (transform.position, target, speed * Time.deltaTime);
+		}
+	}
+}
