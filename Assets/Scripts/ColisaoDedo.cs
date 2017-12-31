40f52573231559f02a040ab254a780ded3823662
Paulo Salvatore <salvatoregames@gmail.com>
2017-12-30 23:33:51 -0200

    Script de colisão do dedo alterado

--- Assets/Scripts/ColisaoDedo.cs
+++ Assets/Scripts/ColisaoDedo.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class ColisaoDedo : MonoBehaviour
 {
-	private bool interacaoLiberada = true;
+	public Transform colliderDedo;
+	private bool instanciarColliderDedoLeft = true;
+	private bool instanciarColliderDedoRight = true;
 
 	private void Start()
 	{
+		StartCoroutine(PosicionarColliderDedos());
 	}
 
-	private void Update()
+	private IEnumerator PosicionarColliderDedos()
 	{
+		while (true)
+		{
+			if (instanciarColliderDedoLeft)
+			{
+				GameObject pontaIndicador = GameObject.Find("hands:b_l_index_ignore");
+
+				if (pontaIndicador != null)
+				{
+					GameObject colliderDedoLeft = Instantiate(colliderDedo.gameObject);
+
+					colliderDedoLeft.transform.SetParent(pontaIndicador.transform);
+
+					colliderDedoLeft.transform.localPosition = colliderDedo.transform.localPosition;
+					colliderDedoLeft.transform.localRotation = colliderDedo.transform.localRotation;
+
+					instanciarColliderDedoLeft = false;
+				}
+			}
+
+			if (instanciarColliderDedoRight)
+			{
+				GameObject pontaIndicador = GameObject.Find("hands:b_r_index_ignore");
+
+				if (pontaIndicador != null)
+				{
+					GameObject colliderDedoRight = Instantiate(colliderDedo.gameObject);
+
+					colliderDedoRight.transform.SetParent(pontaIndicador.transform);
+
+					colliderDedoRight.transform.localPosition = colliderDedo.transform.localPosition;
+					colliderDedoRight.transform.localRotation = colliderDedo.transform.localRotation;
+
+					instanciarColliderDedoRight = false;
+				}
+			}
+
+			if (!instanciarColliderDedoLeft &&
+				!instanciarColliderDedoRight)
+			{
+				Destroy(colliderDedo.gameObject);
+
+				break;
+			}
+
+			yield return null;
+		}
 	}
+
+	/*
 
 	private void LiberarInteracao()
 	{
@@ -57,4 +108,6 @@ public class ColisaoDedo : MonoBehaviour
 				Invoke("LiberarInteracao", 1f);
 		}
 	}
+
+	*/
 }
