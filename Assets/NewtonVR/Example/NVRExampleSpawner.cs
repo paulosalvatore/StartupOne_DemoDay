49fe65a3d9fca5dfa0257aea857ddb185d79240a
Paulo Salvatore <salvatoregames@gmail.com>
2017-12-30 23:30:27 -0200

    Ponto de spawn do script 'ExampleSpawner' alterado

--- Assets/NewtonVR/Example/NVRExampleSpawner.cs
+++ Assets/NewtonVR/Example/NVRExampleSpawner.cs
@@ -4,20 +4,20 @@ using NewtonVR;
 
 namespace NewtonVR.Example
 {
-    public class NVRExampleSpawner : MonoBehaviour
-    {
-        public NVRButton Button;
+	public class NVRExampleSpawner : MonoBehaviour
+	{
+		public NVRButton Button;
 
-        public GameObject ToCopy;
+		public GameObject ToCopy;
 
-        private void Update()
-        {
-            if (Button.ButtonDown)
-            {
-                GameObject newGo = GameObject.Instantiate(ToCopy);
-                newGo.transform.position = this.transform.position + new Vector3(0, 1, 0);
-                newGo.transform.localScale = ToCopy.transform.lossyScale;
-            }
-        }
-    }
+		private void Update()
+		{
+			if (Button.ButtonDown)
+			{
+				GameObject newGo = GameObject.Instantiate(ToCopy);
+				newGo.transform.position = this.transform.position + new Vector3(-1, 1, 0);
+				newGo.transform.localScale = ToCopy.transform.lossyScale;
+			}
+		}
+	}
 }
